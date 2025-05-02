3736fba07d8e7dcc37b4d906f07964dce323d625
Macro <macrochen05@gmail.com>
2025-05-01 22:07:26 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# load dataset
+mnist = tf.keras.datasets.mnist
+# setup initial variables
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# normalizing grey scale data from 0-255 to 0-1
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+# flatten the 28x28 grid to a 784 length array
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# adding neural network layers
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+# softmax makes the sum of the probability of the 10 nodes to = 1
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# train the model
+model.fit(x_train, y_train, epochs=3)
+
+model.save('handwritten.keras')
+
+# load model
+model = tf.keras.models.load_model('handwritten.keras')
+
+image_number = 0
+while os.path.isfile(f"digits/{image_number}.png"):
+    try:
+        img = cv2.imread(f"digits/{image_number}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"This digit is most likely a {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error")
+    finally:
+        image_number += 1
